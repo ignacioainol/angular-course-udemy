f94f98160faae1d735610f37b5b8eeb641c3f6c7
Ignacio Ainol <iggnaxios@gmail.com>
2026-03-03 15:57:44 -0300

    03-gifs Created interfaces and service to get gifs

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
 
+  trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal(true);
+
   constructor() {
     this.loadTrendingGifs();
+    console.log('Service initialized');
   }
 
   loadTrendingGifs() {
@@ -20,7 +26,10 @@ export class GifService {
         },
       })
       .subscribe((resp) => {
-        console.log({ resp });
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
+        console.log(gifs);
       });
   }
 }
